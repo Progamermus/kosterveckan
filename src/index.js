270289668dc0bffbdd20a6f0e2bf11d1598ff76c
Progamermus <rasmushallen@gmail.com>
2018-07-09 20:38:10 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyCHUsQ_f7-2gfGKBtTtCHbrNCt-4dmMAFs",
+   authDomain: "kosterveckan.firebaseapp.com",
+   databaseURL: "https://kosterveckan.firebaseio.com",
+   projectId: "kosterveckan",
+   storageBucket: "kosterveckan.appspot.com",
+   messagingSenderId: "152411542473"
+ };
+ firebase.initializeApp(config);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
